48ecef22b3b3a06e996ee1ddef82a4e37917cc9f
jongmin-oh <alswhddh3@gmail.com>
2025-08-31 14:33:49 +0000

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from kakaotalk-self-replication!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 메신저봇R/code.js
@@ -0,0 +1,8 @@
+
+
+
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    if(msg=="퉤"){
+      replier.reply("퉤");
+    }
+  }
